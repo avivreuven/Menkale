8f4e9d0c2e3ed9f206201df5ddc48a129e7bba56
Aviv Reuven <aviv1204@gmail.com>
2022-02-18 17:57:11 +0200

    print_board function choose_hole function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-def printBoard(board):
+def print_board(board):
     print("Player2      6         5         4         3         2         1      Player1")
     print("|-----|   |-----|   |-----|   |-----|   |-----|   |-----|   |-----|   |-----|")
     print("|     |   |     |   |     |   |     |   |     |   |     |   |     |   |     |")
@@ -14,8 +14,36 @@ def printBoard(board):
     print("|-----|   |-----|   |-----|   |-----|   |-----|   |-----|   |-----|   |-----|")
     print("Player2      7         8         9        10        11        12      Player1")
 
+def choose_hole():
+    hole = int(input("Choose hole (1-12):"))
+    while hole < 1 or hole > 12:
+        hole = int(input("Wrong Number, Choose hole (1-12):"))
+    if hole > 6:
+        hole = hole + 1
+    while board[hole] == 0:
+        hole = int(input("Hole is Empty, Choose another hole"))
+        while hole < 1 or hole > 13:
+            hole = int(input("Wrong Number, Choose hole (1-12):"))
+        if hole > 6:
+            hole = hole + 1
+    return hole
+
 
 board = [0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4]
-printBoard(board)
+print_board(board)
+
+playerTurn = True
+while board[0]+board[7] < 48:
+    if playerTurn:
+        print("\nPlayer 1 turn")
+    else:
+        print("\nPlayer 2 turn")
+    hole = choose_hole()
+    playerTurn = not playerTurn
+
+
+
+
+
 
 
